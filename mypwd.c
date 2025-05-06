6e50fa79860307b51ccc2590e153be598b878932
NadaMamdouh225 <nada20011002@gmail.com>
2025-05-06 07:31:46 +0300

    add pwd utility

---
+++ mypwd.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char **argv)
+{
+    char* buf = getcwd(NULL,0);
+    if( buf == NULL)
+    {
+        perror("Couldn't get current working directory");
+        exit(-1);
+    }
+    printf("%s",buf);
+    free(buf);
+    return 0;
+}
